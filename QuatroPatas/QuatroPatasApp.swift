dec5246bf7eb3a55feb6d2eab46e1585ad735a14
Vinicius Mesquita <viniciuscoelho.ti@gmail.com>
2025-03-21 15:55:02 -0300

    Initial Commit

---
+++ QuatroPatas/QuatroPatasApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuatroPatasApp.swift
+//  QuatroPatas
+//
+//  Created by Vinicius Mesquita Coelho on 21/03/25.
+//
+
+import SwiftUI
+
+@main
+struct QuatroPatasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuatroPatasTests/QuatroPatasTests.swift
@@ -0,0 +1,16 @@
+//
+//  QuatroPatasTests.swift
+//  QuatroPatasTests
+//
+//  Created by Vinicius Mesquita Coelho on 21/03/25.
+//
+
+import Testing
+
+struct QuatroPatasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
