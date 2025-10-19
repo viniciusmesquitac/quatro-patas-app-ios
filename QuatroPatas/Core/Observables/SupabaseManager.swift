927ed8c15d8162bb82e801381285846baedca863
Vinicius Mesquita <viniciuscoelho.ti@gmail.com>
2025-10-19 10:42:58 -0300

    Adiciona credenciais de dev

--- QuatroPatas/Core/Observables/SupabaseManager.swift
+++ QuatroPatas/Core/Observables/SupabaseManager.swift
@@ -12,8 +12,8 @@ import Supabase
 final class SupabaseManager {
     static let shared = SupabaseManager()
     
-    private let supabaseURL = URL(string: "")!
-    private let supabaseKey = ""
+    private let supabaseURL = URL(string: "https://lhonxgkncgneeocannli.supabase.co")!
+    private let supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxob254Z2tuY2duZWVvY2FubmxpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4NDIzMjQsImV4cCI6MjA3NjQxODMyNH0.i2rASSNKhQ5FDR3H-iluVUTrf_J0GPvWasEMKFstmyw"
 
     let client: SupabaseClient
 
